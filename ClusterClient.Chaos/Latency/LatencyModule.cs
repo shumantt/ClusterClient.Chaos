556749e4e22113ef71496bf9780be95efc92f5ec
Andrey Shumeev <shumantt@gmail.com>
2020-11-24 23:04:09 +0300

    provide latency and rate with funcs so it can be hot-configured

--- ClusterClient.Chaos/Latency/LatencyModule.cs
+++ ClusterClient.Chaos/Latency/LatencyModule.cs
@@ -8,31 +8,29 @@ namespace ClusterClient.Chaos.Latency
 {
     public class LatencyModule : IRequestModule
     {
-        private readonly int delayMillisecond;
-        private readonly double probability;
+        private readonly Func<TimeSpan> delayProvider;
+        private readonly Func<double> rateProvider;
 
-        public LatencyModule(int delayMillisecond, double probability)
+        public LatencyModule(Func<TimeSpan> delayProvider, Func<double> rateProvider)
         {
-            if (delayMillisecond < 0)
-            {
-                throw new ArgumentException($"{nameof(delayMillisecond)} should be a positive number");
-            }
-            
-            if (probability < 0 || probability > 1)
-            {
-                throw new ArgumentException($"{nameof(probability)} should be in a range from 0.0 to 1.0");
-            }
-            
-            this.delayMillisecond = delayMillisecond;
-            this.probability = probability;
+            this.delayProvider = delayProvider;
+            this.rateProvider = rateProvider;
+        }
+
+        public LatencyModule(TimeSpan delay, double rate)
+        {
+            delayProvider = () => delay;
+            rateProvider = () => rate;
         }
         
         public async Task<ClusterResult> ExecuteAsync(IRequestContext context, Func<IRequestContext, Task<ClusterResult>> next)
         {
             if (ShouldAddLatency())
             {
-                var delay = Math.Min(delayMillisecond, context.Budget.Remaining.Milliseconds);
-                await Task.Delay(delay, context.CancellationToken).ConfigureAwait(false);
+                var delay = delayProvider();
+                var remainingTimeBudget = context.Budget.Remaining;
+                var latency = delay > remainingTimeBudget ? remainingTimeBudget : delay;
+                await Task.Delay(latency, context.CancellationToken).ConfigureAwait(false);
             }
             
             return await next(context).ConfigureAwait(false);
@@ -40,7 +38,7 @@ namespace ClusterClient.Chaos.Latency
 
         private bool ShouldAddLatency()
         {
-            return ThreadSafeRandom.NextDouble() <= probability;
+            return ThreadSafeRandom.NextDouble() <= rateProvider();
         }
     }
 }
