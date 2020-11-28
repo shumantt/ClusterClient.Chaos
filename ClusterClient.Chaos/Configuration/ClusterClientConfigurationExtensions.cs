16da68a3bda8d35e9651df534f987fbc78b6a330
Andrey Shumeev <shumantt@gmail.com>
2020-11-28 19:48:27 +0300

    configure latency for total, every-retry and every network call

--- ClusterClient.Chaos/Configuration/ClusterClientConfigurationExtensions.cs
+++ ClusterClient.Chaos/Configuration/ClusterClientConfigurationExtensions.cs
@@ -3,25 +3,35 @@ using ClusterClient.Chaos.Latency;
 using Vostok.Clusterclient.Core;
 using Vostok.Clusterclient.Core.Misc;
 using Vostok.Clusterclient.Core.Modules;
+using Vostok.Clusterclient.Core.Strategies;
 
 namespace ClusterClient.Chaos.Configuration
 {
     public static class ClusterClientConfigurationExtensions
     {
-        public static void SetupLatencyChaos(
+        public static void SetupTotalLatency(
+            this IClusterClientConfiguration configuration, 
+            Func<TimeSpan> delayProvider, 
+            Func<double> rateProvider)
+        {
+            configuration.AddRequestModule(new LatencyModule(delayProvider, rateProvider), RequestModule.RequestRetry, ModulePosition.Before);
+        }
+
+        public static void SetupLatencyOnEveryRetry(
             this IClusterClientConfiguration configuration, 
             Func<TimeSpan> delayProvider, 
             Func<double> rateProvider)
         {
             configuration.AddRequestModule(new LatencyModule(delayProvider, rateProvider), RequestModule.RequestRetry, ModulePosition.After);
         }
-        
-        public static void SetupLatencyChaos(
-            this IClusterClientConfiguration configuration, 
-            TimeSpan delay, 
-            double rate)
+
+        public static void SetupLatencyOnEveryNetworkCall(
+            this IClusterClientConfiguration configuration,
+            IRequestStrategy defaultRequestStrategy,
+            Func<TimeSpan> delayProvider,
+            Func<double> rateProvider)
         {
-            SetupLatencyChaos(configuration, () => delay, () => rate);
+            configuration.DefaultRequestStrategy = new LatencyStrategy(delayProvider, rateProvider, defaultRequestStrategy);
         }
     }
 }
